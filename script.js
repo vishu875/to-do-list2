0f56b7afdadb675245faa864f9e2100857ffedd5
Vishal Kumar <vishalgit38@gmail.com>
2026-09-22 18:44:56 +0530

    change

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const task = document.getElementById('input_text')
 const button = document.getElementById('btn')
 
-const Add_List = document.getElementById('Add-list')
+const Add_List = document.getElementById('      Add-list')
 
-button.addEventListener('click', function(){
+button.addEventListener('click',        function(){
   
   task
 
-  const li = document.createElement('li')
+  const li = document.createElement('l i')
   const text = document.
   li.append
 
