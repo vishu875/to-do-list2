7ff25cd60e8b7034e384e3fd354023a910211116
Vishal Kumar <vishalgit38@gmail.com>
2026-09-22 18:42:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const task = document.getElementById('input_text')
+const button = document.getElementById('btn')
+
+const Add_List = document.getElementById('Add-list')
+
+button.addEventListener('click', function(){
+  
+  task
+
+  const li = document.createElement('li')
+  const text = document.
+  li.append
+
+})
